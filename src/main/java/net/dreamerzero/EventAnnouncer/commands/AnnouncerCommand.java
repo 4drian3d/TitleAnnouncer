88fe25a31093fc9349129f02aac1eb81efba1bd9
Adrian Gonzales <adrianed2004@gmail.com>
2021-08-04 12:27:47 -0500

    Removed unused import

--- src/main/java/net/dreamerzero/EventAnnouncer/commands/AnnouncerCommand.java
+++ src/main/java/net/dreamerzero/EventAnnouncer/commands/AnnouncerCommand.java
@@ -12,7 +12,7 @@ import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
+
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.title.Title;
 
--- src/main/java/net/dreamerzero/EventAnnouncer/commands/PruebaCommand.java
+++ src/main/java/net/dreamerzero/EventAnnouncer/commands/PruebaCommand.java
@@ -10,7 +10,6 @@ import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.title.Title;
 
