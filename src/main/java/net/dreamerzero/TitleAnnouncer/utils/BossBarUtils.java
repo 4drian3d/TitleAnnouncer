8a57919181eda325bf91a90ea71c632d64699d0b
4drian3d <adrianed2004@gmail.com>
2021-08-27 09:12:42 -0500

    Initial bossbar implementation

---
+++ src/main/java/net/dreamerzero/TitleAnnouncer/utils/BossBarUtils.java
@@ -0,0 +1,45 @@
+package net.dreamerzero.TitleAnnouncer.utils;
+
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.Component;
+
+public class BossBarUtils {
+    BossBar activeBar;
+
+    public void showBossBar (
+        final Audience audience,
+        final int time,
+        final Component content, 
+        final BossBar.Color color, 
+        final BossBar.Overlay type) {
+        
+        float finalTime = time/100;
+        if (finalTime > 100){
+            finalTime = 100;
+        }
+        final BossBar bar = BossBar.bossBar(
+            content, 
+            finalTime, 
+            color, 
+            type);
+
+        audience.showBossBar(bar);
+        activeBar = bar;
+
+        while (finalTime > 1){
+            bar.progress(finalTime);
+            finalTime--;
+        }
+
+        activeBar = null;
+        
+    }
+
+    public void hideBossBar(
+        final Audience audience) {
+
+        audience.hideBossBar(activeBar);
+        activeBar = null;
+    }
+}
