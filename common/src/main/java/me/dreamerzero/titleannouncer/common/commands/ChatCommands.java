7184c13163636bacedf2d48dc3c6433a62719358
4drian3d <adriangonzalesval@gmail.com>
2022-04-13 17:03:28 +0000

    fix: self argument applied to send command in chat commands

--- common/src/main/java/me/dreamerzero/titleannouncer/common/commands/ChatCommands.java
+++ common/src/main/java/me/dreamerzero/titleannouncer/common/commands/ChatCommands.java
@@ -29,7 +29,7 @@ public class ChatCommands<A>  {
                     })
                 )
             )
-            .then(LiteralArgumentBuilder.<A>literal("send")
+            .then(LiteralArgumentBuilder.<A>literal("self")
                 .then(RequiredArgumentBuilder.<A, String>argument("message", StringArgumentType.string())
                     .executes(cmd -> {
                         adapter.toAudience(cmd.getSource()).sendMessage(
@@ -39,7 +39,7 @@ public class ChatCommands<A>  {
                     })
                 )
             )
-            .then(LiteralArgumentBuilder.<A>literal("self")
+            .then(LiteralArgumentBuilder.<A>literal("send")
                 .then(RequiredArgumentBuilder.<A, String>argument("objetive", StringArgumentType.word())
                     .then(RequiredArgumentBuilder.<A, String>argument("message", StringArgumentType.string())
                         .executes(cmd -> {
