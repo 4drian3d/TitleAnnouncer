2ce4db740d224d8549bbc3af932dff1e0eab84cb
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-10-13 19:52:18 -0500

    fix: Updated actions

---
+++ paper/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+module io.github._4drian3d.titleannouncer.paper {
+  requires com.google.guice;
+  requires io.github._4drian3d.titleannouncer.common;
+  requires javax.inject;
+  requires net.kyori.adventure;
+  requires org.bukkit;
+  requires static org.jetbrains.annotations;
+  requires static transitive org.jspecify;
+}
